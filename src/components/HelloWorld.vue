ed93a1fcda338c024eb96d8d7535e00fb1846600
Mihai <haos-sah@mail.ru>
2020-04-26 15:03:35 +0300

    vuetify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,141 +1,114 @@
-<template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
-</template>
-
 <script>
 export default {
   name: "HelloWorld",
 
   data: () => ({
-    ecosystem: [
+    desserts: [
       {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader"
+        name: 'Frozen Yogurt',
+        calories: 159,
       },
       {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify"
+        name: 'Ice cream sandwich',
+        calories: 237,
       },
       {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify"
-      }
+        name: 'Eclair',
+        calories: 262,
+      },
+      {
+        name: 'Cupcake',
+        calories: 305,
+      },
+      {
+        name: 'Gingerbread',
+        calories: 356,
+      },
+      {
+        name: 'Jelly bean',
+        calories: 375,
+      },
+      {
+        name: 'Lollipop',
+        calories: 392,
+      },
+      {
+        name: 'Honeycomb',
+        calories: 408,
+      },
+      {
+        name: 'Donut',
+        calories: 452,
+      },
+      {
+        name: 'KitKat',
+        calories: 518,
+      },
     ],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com"
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com"
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify"
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs"
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify"
-      }
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer"
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/layout/pre-defined"
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
-      }
-    ]
   })
-};
+}
 </script>
+
+<template>
+  <v-container>
+    <div>
+      <v-text-field
+        label="primabvara">
+      </v-text-field>
+
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+          <tr>
+            <th class="text-left">Name</th>
+            <th class="text-left">Calories</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="item in desserts" :key="item.name">
+            <td>{{ item.name }}</td>
+            <td>{{ item.calories }}</td>
+          </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+
+     <div class="d-fex">
+       <div >
+         <v-btn color="default">Primary</v-btn>
+       </div>
+       <div >
+         <v-btn color="secondary">Primary</v-btn>
+       </div>
+       <div >
+         <v-btn color="info">Primary</v-btn>
+       </div>
+       <div class="ml-2">
+         <v-btn color="success">Primary</v-btn>
+       </div>
+       <div class="ml-2">
+         <v-btn  color="primary">Primary</v-btn>
+       </div>
+       <div class="ml-2">
+         <v-btn  color="warning">Primary</v-btn>
+       </div>
+       <div class="ml-2">
+         <v-btn  color="error">Primary</v-btn>
+       </div>
+     </div>
+
+      <v-card>
+        <div style="padding: 50px 0"  class="card">
+          aaaaa goriu
+        </div>
+      </v-card>
+
+    </div>
+  </v-container>
+</template>
+
+<style scoped>
+  .card {
+    background: var(--v-secondary-base);
+  }
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -10,8 +10,19 @@ export default new Vuetify({
     },
     themes: {
       light: {
-        primary: "#ee44aa",
-        secondary: "#424242",
+        default: "#999999",
+        primary: "#3a5dee",
+        secondary: "#9C27B0",
+        accent: "#82B1FF",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107"
+      },
+      dark: {
+        default: "#999999",
+        primary: "#3a5dee",
+        secondary: "#FF0000",
         accent: "#82B1FF",
         error: "#FF5252",
         info: "#2196F3",
